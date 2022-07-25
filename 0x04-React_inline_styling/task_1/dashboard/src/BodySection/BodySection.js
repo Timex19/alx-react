71dc5b4d1c645812cc83417c078651cca7707d15
Timex19 <ojooluwatimilehinpeter@yahoo.com>
2022-07-25 21:24:52 +0100

    Adding all files for task 1

--- 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySection.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySection.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import './BodySection.css';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { StyleSheet, css } from "aphrodite";
 
 class BodySection extends Component {
   constructor(props) {
@@ -10,8 +10,8 @@ class BodySection extends Component {
   render() {
     const { children, title } = this.props;
     return (
-      <div className='bodySection'>
-        <h2>{title}</h2>
+      <div className={css(styles.bodySection)}>
+        <h2 className={css(styles.bodySectionH2)}>{title}</h2>
         {children}
       </div>
     );
@@ -19,11 +19,23 @@ class BodySection extends Component {
 }
 
 BodySection.defaultProps = {
-  title: '',
+  title: "",
 };
 
 BodySection.propTypes = {
   title: PropTypes.string,
 };
+
+const styles = StyleSheet.create({
+  bodySection: {
+    display: "flex",
+    flexWrap: "wrap",
+    width: "100%",
+  },
+
+  bodySectionH2: {
+    width: "100%",
+  },
+});
 
 export default BodySection;
